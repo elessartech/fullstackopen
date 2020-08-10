48c5afeb2929978287c9237b32b1a8c98212fc65
IlmastMaksim <ilmastm9@gmail.com>
2020-08-10 19:27:02 +0300

    4.15 done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
@@ -7,17 +8,28 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const content = new Blog(request.body);
-  const parsedContent = JSON.parse(JSON.stringify(content));
+  const body = request.body;
+  const user = await User.findById(body.userId);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
+  });
+
+  const parsedBlog = JSON.parse(JSON.stringify(blog));
   if (
-    !parsedContent.hasOwnProperty("likes") ||
-    (!parsedContent.hasOwnProperty("url") &&
-      !parsedContent.hasOwnProperty("title"))
+    !parsedBlog.hasOwnProperty("likes") ||
+    (!parsedBlog.hasOwnProperty("url") && !parsedBlog.hasOwnProperty("title"))
   ) {
     response.status(400).end();
   } else {
-    const result = await content.save();
-    response.status(201).json(result);
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+    response.status(201).json(savedBlog);
   }
 });
 
