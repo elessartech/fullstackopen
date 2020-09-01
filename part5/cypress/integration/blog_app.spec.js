5a26d6db063be790e836b1a979a55bbe36de5165
IlmastMaksim <ilmastm9@gmail.com>
2020-09-01 12:17:21 +0300

    5.20 done

--- part5/cypress/integration/blog_app.spec.js
+++ part5/cypress/integration/blog_app.spec.js
@@ -40,6 +40,32 @@ describe("Blog app", function () {
   });
 
   describe.only("When logged in", function () {
+
+    Cypress.Commands.add('login', ({ username, password }) => {
+      cy.request('POST', 'http://localhost:3001/api/login', {
+        username, password
+      }).then(({ body }) => {
+        localStorage.setItem('loggedBlogappUser', JSON.stringify(body))
+        cy.visit('http://localhost:3000')
+        cy.get("#username-input").type("test");
+        cy.get("#password-input").type("test");
+        cy.get("#login-button").click();
+      })
+    })
+
+    Cypress.Commands.add('createBlog', ({ title, author, url }) => {
+      cy.request({
+        url: 'http://localhost:3001/api/blogs/',
+        method: 'POST',
+        body: { title, author, url },
+        headers: {
+          'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedBlogappUser')).token}`
+        }
+      })
+    
+      cy.visit('http://localhost:3000')
+    })
+    
     beforeEach(function () {
       cy.request("POST", "http://localhost:3001/api/testing/reset");
       const user = {
@@ -48,14 +74,7 @@ describe("Blog app", function () {
         password: "test",
       };
       cy.request("POST", "http://localhost:3001/api/users/", user);
-      cy.request("POST", "http://localhost:3001/api/login/", {
-        username: user.username,
-        password: user.password,
-      });
-      cy.visit("http://localhost:3000");
-      cy.get("#username-input").type("test");
-      cy.get("#password-input").type("test");
-      cy.get("#login-button").click();
+      cy.login({ username: user.username, password: user.password })
     });
 
     it("A blog can be created", function () {
@@ -69,7 +88,24 @@ describe("Blog app", function () {
       cy.get("#title").type(blog.title);
       cy.get("#url").type(blog.url);
       cy.contains("save").click();
+
       cy.contains(blog.title)
-    });
+      })
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'My Test Blog',
+          author: "Test Author",
+          url: "google.com"
+        })
+      })
+
+      it("A blog can be liked", function () {
+        cy.contains("view").click();
+        cy.contains("like").click();
+        cy.contains("you liked the post")
+      });
+  });
   });
 });
